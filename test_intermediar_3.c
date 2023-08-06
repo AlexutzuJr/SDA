5caa838f2d8404a6e4986bdb2ce3b4e241986057
Alexandru-Nicolae Dinu <107935470+AlexutzuJr@users.noreply.github.com>
2023-08-06 22:04:20 +0300

    Create test_intermediar_3.c

---
+++ test_intermediar_3.c
@@ -0,0 +1,24 @@
+/* Se dă o listă de date simplu înlănțuită identificată prin pointer către primul element, numit head. Nodurile au următoarea formă:
+
+   struct node
+   {
+       int val;
+       struct node *next;
+   }
+
+   Să se scrie o funcție care afișează elementele listei în ordinea inversă față de cea în care se află înlănțuite, câte o valoare pe rând, urmată de caracterul newline. 
+   Funcția are următorul prototip: void reverse_display(struct node *head); */
+
+
+  void reverse_display(struct node* head)
+{
+  if(head==NULL)
+{
+return;
+}
+
+reverse_display(head->next);
+printf("%d head->val);
+  }
+
+  
